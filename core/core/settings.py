be59201899e6b47ffd5a4375fb4baf3f0684f837
MahoneyT1 <ifeanyitech15@gmail.com>
2025-02-19 12:08:17 +0100

    added requirement.txt file

--- core/core/settings.py
+++ core/core/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-website-4-5jf1.onrender.com']
+ALLOWED_HOSTS = ['portfolio-website-4-5jf1.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
