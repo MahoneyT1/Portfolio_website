d04b61a8d8551287072283f0b1351e8b7fc4c3d4
MahoneyT1 <ifeanyitech15@gmail.com>
2025-02-19 08:00:42 +0100

    restructured the project folder

---
+++ core/src/staticfiles/js/home.js
@@ -0,0 +1,32 @@
+// /**
+//  * Home page hero section function Hero slider
+//  * function 
+//  */
+
+// // get the elements my for styling using quarySelectorAll
+
+
+// export default function HeroSlider() {
+//     document.addEventListener("DOMContentLoaded", ()=> {
+//         const slides = document.querySelectorAll(".slide");
+
+//         let currentIndex = 0;
+    
+//         const switchSlide = ()=> {
+    
+//             slides[currentIndex].classList.remove("active");
+//             currentIndex = (currentIndex + 1) % slides.length;
+//             slides[currentIndex].classList.add("active");
+//         };
+//         setInterval(switchSlide, 3000);
+
+//     });
+// };
+
+// HeroSlider();
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log('Hello-world')
+    
+})
