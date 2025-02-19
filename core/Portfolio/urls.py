796007fbb00bffec2601ac6c8921c663ec97c75c
MahoneyT1 <ifeanyitech15@gmail.com>
2025-02-19 08:19:01 +0100

    added requirement.txt file

--- core/Portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, about, portfolio, contact
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('about/', about, name="about"),
-    path('portfolio/', portfolio, name="portfolio"),
-    path('contact/', contact, name="contact")
-]
